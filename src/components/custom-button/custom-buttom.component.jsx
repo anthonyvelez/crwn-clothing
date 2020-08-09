e7c7ea0894f0b7c7136dab7f4b9b15062c015901
Anthony Velez <velezanthony1@gmail.com>
2020-08-08 20:22:01 -0600

    update cart utils to group duplicate items

--- src/components/custom-button/custom-buttom.component.jsx
+++ src/components/custom-button/custom-buttom.component.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({
+	children,
+	isGoogleSignIn,
+	inverted,
+	...otherProps
+}) => (
 	<button
-		className={`${isGoogleSignIn
-			? 'google-sign-in'
+		className={`${isGoogleSignIn ? 'google-sign-in' : ''} ${inverted
+			? 'inverted'
 			: ''} custom-button`}
 		{...otherProps}
 	>
